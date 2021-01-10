9335ff98ca445f2a9df667fb46cb2b117dd23631
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-01-11 00:48:15 +0900

    10872_팩토리얼

---
+++ factorial/src/factorial/Main.java
@@ -0,0 +1,33 @@
+package factorial;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int num = Integer.parseInt(br.readLine());
+			int result = 1;
+			if(num != 0) {
+				for(int i = 1; i <= num; i++) {
+					result *= i;
+				}
+			}
+			bw.write(result);
+			bw.close();
+			br.close();
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
