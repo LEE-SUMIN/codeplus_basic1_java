c92383988ca588602224aaecf2b25715b15fc6d7
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-01-11 23:33:26 +0900

    1676_팩토리얼 0의 개수

---
+++ math7/src/math7/Main.java
@@ -0,0 +1,34 @@
+package math7;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		try {
+			int n = Integer.parseInt(br.readLine());
+			int k = 5;
+			int result = 0;
+			while(k <= n) {
+				result += (n / k);
+				k *= 5;
+			}
+			sb.append(result);
+			bw.write(sb.toString());
+			bw.close();
+			br.close();
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
