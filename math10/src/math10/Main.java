7adab9054298c4db596a9a449c0953ed25c4fb8e
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-01-12 21:29:04 +0900

    17087_숨박꼭질 6

--- math10/src/math10/Main.java
+++ math10/src/math10/Main.java
@@ -40,4 +40,53 @@ public class Main {
 		}
 	}
 	
+	/*
+	 public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokenizer;
+		
+		try {
+			tokenizer = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(tokenizer.nextToken());
+			int S = Integer.parseInt(tokenizer.nextToken());
+			int[] distance = new int[N];
+			int min = 0;
+			tokenizer = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; i++) {
+				int d = Math.abs(S - Integer.parseInt(tokenizer.nextToken()));
+				if(min == 0 || d < min) {
+					min = d;
+				}
+				distance[i] = d;
+			}
+			int result = find_gcd(min, distance);
+			
+			System.out.println(result);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static int find_gcd(int n, int[] distance) {
+		for(int divisor = n; divisor > 0; divisor--) {
+			if(n % divisor == 0) {
+				boolean status = true;
+				for(int i = 0; i < distance.length; i++) {
+					if(distance[i] % divisor != 0) {
+						status = false;
+						break;
+					}
+				}
+				if(status) {
+					return divisor;
+				}
+			}
+		}
+		return -1;
+	}
+}
+	 */
+	
 }
