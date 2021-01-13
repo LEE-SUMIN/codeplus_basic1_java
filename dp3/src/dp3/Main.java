fc18fef3fe2c78c4bf4f23f5fe305427b70c13b5
LEE-SUMIN <37135336+LEE-SUMIN@users.noreply.github.com>
2021-01-13 11:54:53 +0900

    11727_2*n 타일링 2

---
+++ dp3/src/dp3/Main.java
@@ -0,0 +1,49 @@
+package dp3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int N = Integer.parseInt(br.readLine());
+			Calculation c = new Calculation();
+			bw.append(c.calc(N) + "");
+			
+			bw.close();
+			br.close();
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
+
+class Calculation{
+	int[] d = new int [1001];
+	Calculation(){
+		d[0] = 1;
+		d[1] = 1;
+		d[2] = 3;
+	}
+	
+	public int calc(int n) {
+		if(d[n] > 0)
+			return d[n];
+		else {
+			d[n] = (calc(n - 1) + 2 * calc(n - 2)) % 10007;
+		}
+		return d[n];
+	}
+}
